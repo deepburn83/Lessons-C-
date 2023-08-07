3973bcec74d79dfe1e3835837022c0d1e6be1f70
Victor Gudiloff <deepburn1983@gmail.com>
2023-08-07 19:25:57 +0300

    Example011_ArrayLibrary Project

---
+++ Examples/Example011_ArrayLibrary/Program.cs
@@ -0,0 +1,51 @@
+﻿void FillArray(int[] collection)
+//void - метод (функция), который не возвращает.
+// при этом, в конце, не используется команда return
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++; //index = index +1;
+    }
+}
+
+ void PrintArray(int[] col) // метод void, который будет печатать массив
+ {
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+ }
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while (index < count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[10]; //Создание массива длиной 10 элементов
+
+FillArray(array); // метод, заполняющий массив числами
+array[4] = 4;
+array[6] = 4;
+PrintArray(array); //метод, распечатывающий массив
+Console.WriteLine();
+
+int pos = IndexOf(array, 444);
+Console.WriteLine(pos);
